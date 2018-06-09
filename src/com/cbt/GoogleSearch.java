344949e0960812ba280a1e25ad51de8cd5033a98
Melisa <melisa@Melisas-MacBook-Pro.local>
2018-06-09 17:19:44 -0400

    first commit

---
+++ src/com/cbt/GoogleSearch.java
@@ -0,0 +1,20 @@
+package com.cbt;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleSearch {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver",
+				"/Users/melisa/Documents/selenium dependencies/drivers/chromedriver");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.etsy.com/");
+	//driver.close();
+	
+
+	}
+
+}
